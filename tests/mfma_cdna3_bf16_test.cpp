90aee4a44bf8b45b16c1f39841818f20506e0dfc
Benoit Jacob <benoit.jacob@amd.com>
2026-06-17 15:29:57 -0400

    Split heavy MFMA test translation units

--- tests/mfma_cdna3_bf16_test.cpp
+++ tests/mfma_cdna3_bf16_test.cpp
@@ -3,13 +3,13 @@
 
 #include "mfma_cdna3_parts.hpp"
 
-DENSE_VEC_TRAITS(MfmaF16_16x16x16,
+DENSE_VEC_TRAITS(MfmaBF16_1k_16x16x16,
                  16,
                  16,
                  16,
                  1,
                  16,
-                 v4h_native,
-                 v4h_native,
+                 v4bf_native,
+                 v4bf_native,
                  v4f_native,
-                 amdgcn_mfma_f32_16x16x16f16)
+                 amdgcn_mfma_f32_16x16x16bf16_1k)
--- tests/mfma_cdna3_f16_16x16x4_test.cpp
+++ tests/mfma_cdna3_f16_16x16x4_test.cpp
@@ -3,13 +3,13 @@
 
 #include "mfma_cdna3_parts.hpp"
 
-DENSE_VEC_TRAITS(MfmaF16_16x16x16,
+DENSE_VEC_TRAITS(MfmaF16_16x16x4,
                  16,
                  16,
-                 16,
-                 1,
+                 4,
+                 4,
                  16,
                  v4h_native,
                  v4h_native,
-                 v4f_native,
-                 amdgcn_mfma_f32_16x16x16f16)
+                 v16f_native,
+                 amdgcn_mfma_f32_16x16x4f16)
